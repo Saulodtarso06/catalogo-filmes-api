f9400174516888ee8247596854a9225d2b31b02f
Saulo de Tarso <saulo.detarso06@yahoo.com.br>
2025-09-12 17:56:11 -0300

    Adiciona comentários Swagger nas rotas de filmes

--- src/routes/filmes.js
+++ src/routes/filmes.js
@@ -9,3 +9,120 @@ router.put('/:id', filmesController.atualizarFilme);
 router.delete('/:id', filmesController.deletarFilme);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * /filmes:
+ *   get:
+ *     summary: Lista todos os filmes
+ *     tags: [Filmes]
+ *     responses:
+ *       200:
+ *         description: Lista de filmes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Filme'
+ */
+router.get('/', filmesController.listarTodos);
+
+/**
+ * @swagger
+ * /filmes/{id}:
+ *   get:
+ *     summary: Busca um filme pelo ID
+ *     tags: [Filmes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do filme
+ *     responses:
+ *       200:
+ *         description: Filme encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Filme'
+ *       404:
+ *         description: Filme não encontrado
+ */
+router.get('/:id', filmesController.buscarPorId);
+
+/**
+ * @swagger
+ * /filmes:
+ *   post:
+ *     summary: Adiciona um novo filme
+ *     tags: [Filmes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Filme'
+ *     responses:
+ *       201:
+ *         description: Filme criado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Filme'
+ */
+router.post('/', filmesController.criarFilme);
+
+/**
+ * @swagger
+ * /filmes/{id}:
+ *   put:
+ *     summary: Atualiza um filme existente
+ *     tags: [Filmes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do filme
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Filme'
+ *     responses:
+ *       200:
+ *         description: Filme atualizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Filme'
+ *       404:
+ *         description: Filme não encontrado
+ */
+router.put('/:id', filmesController.atualizarFilme);
+
+/**
+ * @swagger
+ * /filmes/{id}:
+ *   delete:
+ *     summary: Remove um filme
+ *     tags: [Filmes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do filme
+ *     responses:
+ *       204:
+ *         description: Filme removido com sucesso
+ *       404:
+ *         description: Filme não encontrado
+ */
+router.delete('/:id', filmesController.deletarFilme);
